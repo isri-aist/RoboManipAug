29f0c65a99159bdcda7a19d89c2042d301a9fe2c
Masaki Murooka <m-murooka@aist.go.jp>
2024-12-09 12:25:09 +0900

    Add core files.

---
+++ README.md
@@ -0,0 +1,2 @@
+# [RoboManipAug](https://github.com/isri-aist/RoboManipAug)
+Data augmentation for robust imitation learning of robot manipulation
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from distutils.util import convert_path
+
+# https://stackoverflow.com/a/24517154
+main_ns = {}
+version_path = convert_path("robo_manip_aug/version.py")
+with open(version_path) as version_file:
+    exec(version_file.read(), main_ns)
+
+setup(
+    name="robo_manip_aug",
+    version=main_ns["__version__"],
+)
